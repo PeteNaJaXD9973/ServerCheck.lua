3f8595fb787ffac6aac59157515365db391c9242
PeteNaJaXD9973 <127975577+PeteNaJaXD9973@users.noreply.github.com>
2023-03-15 23:19:52 +0700

    Create ServerCheck.lua

---
+++ ServerCheck.lua
@@ -0,0 +1,6 @@
+if game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709149431" or game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709149052" or game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709143733" then
+
+end
+if game.Workspace._WorldOrigin.Locations:FindFirstChild('Mirage Island') then
+
+end
