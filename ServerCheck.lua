92b4d834d219ee1ba027e2188aa68e8088770bbb
PeteNaJaXD9973 <127975577+PeteNaJaXD9973@users.noreply.github.com>
2023-03-15 23:32:07 +0700

    Update ServerCheck.lua

--- ServerCheck.lua
+++ ServerCheck.lua
@@ -1,6 +1,8 @@
+pcall(function()
 if game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709149431" or game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709149052" or game:GetService("Lighting"):FindFirstChildOfClass('Sky').MoonTextureId == "http://www.roblox.com/asset/?id=9709143733" then
-
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PeteNaJaXD9973/ServerCheck.lua/main/CFrameMoon.lua'))()
 end
 if game.Workspace._WorldOrigin.Locations:FindFirstChild('Mirage Island') then
-
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PeteNaJaXD9973/ServerCheck.lua/main/CFrameMirage.lua'))()
 end
+end)
